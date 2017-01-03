b93c975a031464e72eade3507d16242ddd60eee0
Fredrik Sudmann <fsudmann@gmail.com>
2017-01-03 15:01:09 +0100

    Delt opp i filer

--- fmg_ethernet_mode.rb
+++ fmg_ethernet_mode.rb
@@ -1,24 +1,5 @@
 #!/usr/bin/env ruby
-#require 'network'
-
-module Networking
-    
-    def show_ip(interface)
-        puts %x(ip a show dev #{interface} | grep inet) + "\n"
-    end
-    def get_speed(interface)
-        %x(ethtool #{interface} | grep Speed 2>/dev/null)
-    end
-
-    def get_duplex(interface)
-        %x(ethtool #{interface} | grep Duplex 2>/dev/null)
-    end
-
-end
-
-
-current = "Current IP Configuration"
-eth1 = "enp8s0"
+require 'network'
 
 class EthernetMode
     include Networking
--- network.rb
+++ network.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
-module Network
+
+module Networking
+    
     def show_ip(interface)
         puts %x(ip a show dev #{interface} | grep inet) + "\n"
     end
-
     def get_speed(interface)
         %x(ethtool #{interface} | grep Speed 2>/dev/null)
     end
@@ -12,8 +13,4 @@ module Network
         %x(ethtool #{interface} | grep Duplex 2>/dev/null)
     end
 
-    def eth_mode(interface, speed, duplex)
-        %x(sudo ethtool -s #{interface} speed #{speed} duplex #{duplex})
-        get_speed(interface)
-    end
 end
