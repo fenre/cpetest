a2408cea5ca5bdec4588339be68657b299b65a97
Fredrik Sudmann <fsudmann@gmail.com>
2017-01-03 11:00:13 +0100

    Objectifying

--- fmg_ethernet_mode.rb
+++ fmg_ethernet_mode.rb
@@ -1,41 +1,55 @@
 #!/usr/bin/env ruby
 #require 'network'
 
+module Networking
+    
+    def show_ip(interface)
+        puts %x(ip a show dev #{interface} | grep inet) + "\n"
+    end
+    def get_speed(interface)
+        %x(ethtool #{interface} | grep Speed 2>/dev/null)
+    end
+
+    def get_duplex(interface)
+        %x(ethtool #{interface} | grep Duplex 2>/dev/null)
+    end
+
+end
+
+
 current = "Current IP Configuration"
-interface = "enp8s0"
+eth1 = "enp8s0"
 
-def show_ip(interface)
-    puts %x(ip a show dev #{interface} | grep inet) + "\n"
+class EthernetMode
+    include Networking
+    
+    def initialize(interface="enp8s0")
+        @interface = interface
+        puts "Starting tests. "
+    end
+
+    def print_status
+        puts "Settings on interface #{@interface} after configuration:"
+        puts get_speed(@interface)
+        puts get_duplex(@interface)
+        puts "-" * 10
+    end
+
+    def set_mode(speed, duplex)    
+        %x(sudo ethtool -s #{@interface} speed #{speed} duplex #{duplex})
+        puts "Configuring #{@interface} to #{speed}MB/s and #{duplex} duplex. \n..."
+        sleep(3)
+        print_status
+    end
 end
 
-def get_speed(interface)
-    %x(ethtool #{interface} | grep Speed 2>/dev/null)
-end
+first = EthernetMode.new
+first.print_status
 
-def get_duplex(interface)
-    %x(ethtool #{interface} | grep Duplex 2>/dev/null)
-end
-
-def print_status(interface)
-    puts "Settings on interface #{interface} after configuration:"
-    puts get_speed(interface)
-    puts get_duplex(interface)
-    puts "-------------------"
-end
-
-def set_mode(interface, speed, duplex)    
-    %x(sudo ethtool -s #{interface} speed #{speed} duplex #{duplex})
-    puts "Configuring #{interface} to #{speed}MB/s and #{duplex} duplex. \n..."
-    sleep(3)
-    print_status(interface)
-end
-
-puts "Starting tests. "
-show_ip(interface)
-
-set_mode(interface,10,"full")
-set_mode(interface,10,"half")
-set_mode(interface,100,"full")
-set_mode(interface,100,"half")
-set_mode(interface,1000,"full")
+#show_ip(interface)
+#set_mode(interface,10,"full")
+#set_mode(interface,10,"half")
+#set_mode(interface,100,"full")
+#set_mode(interface,100,"half")
+#set_mode(interface,1000,"full")
 
